6aeec51d4fcc3791c5193768ca40d043d37d2217
Julius Reuer <juliusreuer2@gmail.com>
2025-05-13 15:35:35 +0200

    Re Structure and added Placeables

--- scripts/Collectable Resources/Ability.gd
+++ scripts/Collectable Resources/Ability.gd
@@ -2,5 +2,7 @@ class_name Ability
 extends Resource
 @export var id: String
 @export var gates: int
+## Chance of placing this Ability in an Important spot: 1/spread (0 <= always)
+@export var spread: int
 
 @export_enum("Early Game", "Midd Game", "End Game") var game_play_time_stamp: int
--- scripts/Dungeon/collectable generator/CollectableGenerator.gd
+++ scripts/Dungeon/collectable generator/CollectableGenerator.gd
@@ -6,24 +6,23 @@ extends DungeonPipeline
 @export var end_game_floor: int = 20
 
 @export var abilitys: Array[Ability]
-#@export var item:Array[Item]
-#@export var upgrad:Array[Upgrade]
-#@export var treasure:Array[Treasure]
+@export var items: Array[Item]
+@export var upgrades: Array[Upgrade]
+@export var treasures: Array[Treasure]
 
 var layout: DungeonLayout
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
-var key_list: Dictionary[String,String] = {}
-var ability_list: Dictionary[String,String] = {}
 var important_spots: Array[String] = []  # Treasure / Upgrades / Abilities
 var ability_check: Array[String] = []
 var item_spots: Array[String] = []
 
+var block_door: Array[String] = []
+var key_placements: Array[String] = []
+var ability_placements: Dictionary[String,String] = {}  #Ability_id,Room
+var door_key_list: Dictionary[String,String] = {}  #Door,Key
 
-func start(dungeon: Dungeon) -> void:
-	layout = dungeon.layout
-	rng.seed = dungeon.rng.randi()
-	generate()
+#region Utill
 
 
 func get_path_to_root(came_from: Dictionary[String,String], start_room: String) -> Array[String]:
@@ -33,20 +32,6 @@ func get_path_to_root(came_from: Dictionary[String,String], start_room: String) 
 		path.append(current)
 		current = came_from.get(current, null)
 	return path
-
-
-func generate() -> void:
-	for leaf in layout.dead_end:
-		var branch = get_branch_path(leaf)
-		if branch.size() <= 2:
-			add_key(leaf)
-		else:
-			important_spots.append(leaf)
-			ability_check.append(branch[1])
-			item_spots.append(branch[2])
-
-	for ability in abilitys:
-		add_ability(ability)
 
 
 func get_branch_path(start_node: String) -> Array[String]:
@@ -69,23 +54,70 @@ func get_branch_path(start_node: String) -> Array[String]:
 	return visited
 
 
-func add_key(leaf: String):
-	var queue: Array[String] = [leaf]
+func generate_gate(key_node: String) -> String:
+	var queue: Array[String] = [key_node]
 	var possible_node_placing: Array[String] = layout.master_room_dict.duplicate().keys()
-	while queue.is_empty():
+	while not queue.is_empty():
 		var current = queue.pop_back()
 		var path = get_path_to_root(layout.to_start_map, current)
 		for node in path:
-			if key_list.has(node):
-				queue.append(key_list[node])
+			if door_key_list.has(node):
+				queue.append(door_key_list[node])
 			possible_node_placing.erase(node)
+	for node in block_door:
+		possible_node_placing.erase(node)
+	return possible_node_placing[rng.randi_range(0, len(possible_node_placing) - 1)]
 
-	var door_pos = possible_node_placing[rng.randi_range(0, len(possible_node_placing) - 1)]
-	key_list.set(door_pos, leaf)
+
+#endregion
+
+
+func start(dungeon: Dungeon) -> void:
+	layout = dungeon.layout
+	rng.seed = dungeon.rng.randi()
+	generate()
+
+
+func generate() -> void:
+	for leaf in layout.dead_end:
+		var branch = get_branch_path(leaf)
+		if branch.size() <= 2:
+			key_placements.append(leaf)
+		else:
+			important_spots.append(leaf)
+			ability_check.append(branch[1])
+			block_door.append(branch[1])
+			item_spots.append(branch[2])
+			block_door.append(branch[2])
+		block_door.append(leaf)
+
+	for key_node in key_placements:
+		add_key(key_node)
+
+	for ability in abilitys:
+		add_ability(ability)
+
+	for item in items:
+		pass  #simple random Place in Area with chance of important room
+
+	for upgrade in upgrades:
+		pass  #simple random Place with chance of important room
+
+	for spot in important_spots:
+		pass  # fill rest of important spots with random treasures
+
+	for treasure in treasures:
+		pass  # spread treasures trough out the dungeon
+
+
+func add_key(leaf: String):
+	var door_pos = generate_gate(leaf)
+	door_key_list.set(door_pos, leaf)
+	block_door.append(door_pos)
 
 
 func add_ability(ability: Ability):
-	var possible_node_placing: Array[String] = layout.master_room_dict.duplicate().keys()
+	var possible_ability_placing: Array[String] = layout.master_room_dict.duplicate().keys()
 	var game_stage_filter = func(node_name: String):
 		match ability.game_play_time_stamp:
 			0:
@@ -95,6 +127,25 @@ func add_ability(ability: Ability):
 				return cur_floor >= mid_game_floor and cur_floor < end_game_floor
 			2:
 				return DungeonLayoutNode.get_floor(node_name) >= end_game_floor
-	possible_node_placing.filter(game_stage_filter)
-	print(possible_node_placing)
-	#for
+	possible_ability_placing = possible_ability_placing.filter(game_stage_filter)
+	for node in block_door:
+		possible_ability_placing.erase(node)
+
+	var filter_important = important_spots.duplicate().filter(game_stage_filter)
+
+	var ability_node: String = ""
+	if len(filter_important) > 0 and rng.randi_range(0, max(ability.spread, 0)) == 0:
+		#Place on important -> cant bee Door
+		ability_node = filter_important[rng.randi_range(0, len(filter_important) - 1)]
+		important_spots.erase(ability_node)
+	else:  #Place Random -> can be door so filter on block door + add to block Door
+		ability_node = possible_ability_placing[rng.randi_range(
+			0, len(possible_ability_placing) - 1
+		)]
+		block_door.append(ability_node)
+
+	for gate_id in ability.gates:
+		var gate = generate_gate(ability_node)
+		block_door.append(gate)
+		door_key_list.set(gate, ability_node)
+	ability_placements.set(ability.id, ability_node)
